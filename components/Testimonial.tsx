685b2c0b5904ba5ef7ce54759fc813fb9ada8474
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-25 21:43:29 +0100

    hjkjbbnm

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -7,7 +7,7 @@ export default function ImageContent1() {
   return (
     <div className="text-[#F7F2DA] p-8 gap-4 space-y-12">
       {/* First Card */}
-      <div className="flex flex-col md:flex-row gap-10">
+      <div className="flex flex-col md:flex-row gap-20">
         <motion.div
           className="flex-1"
           initial={{ x: -100, opacity: 0 }}
@@ -40,49 +40,34 @@ export default function ImageContent1() {
             </motion.span>
           </motion.p>
 
-          <div className="border border-slate-500 bg-black rounded-lg p-4 shadow-lg">
+          <div className="border border-slate-500 bg-black   py-4 shadow-lg">
             <motion.p
-              className="leading-10 tracking-tight px-4 py-2 text-left hover:text-gray-500 text-md md:text-xl hover:scale-110 hover:text-xl md:hover:text-xl hover:-translate-y-1 transition-all duration-300 ease-in-out text-xl font-normal relative"
+              className="leading-10 tracking-tight px-4 py-2 text-left hover:text-gray-500 text-md md:text-xl hover:scale-110 hover:text-xl md:hover:text-xl hover:-translate-y-1 transition-all duration-300 ease-in-out text-xl font-normal "
               whileHover={{
                 y: [-2, 2, -2],
                 transition: { repeat: Infinity, duration: 0.5 }
               }}
             >
               @ZENO_WEB
-              <motion.span
-                className="absolute inset-0 text-transparent pointer-events-none"
-                style={{
-                  textShadow: `
-                    0 0 20px rgba(247, 242, 218, 0.7),
-                    0 0 40px rgba(247, 242, 218, 0.5),
-                    0 0 60px rgba(247, 242, 218, 0.3)
-                  `,
-                  WebkitTextStroke: "2px rgba(247, 242, 218, 0.2)",
-                }}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2, duration: 0.5 }}
-              >
-                @ZENO_WEB
-              </motion.span>
+            
             </motion.p>
             <hr className='bg-slate-500 border-slate-500 w-full mb-4' />
-            <div className="flex items-center justify-between gap-4 mb-4">
+            <div className="flex items-center justify-between gap-4 mb-4 px-4">
               <div className="flex items-center gap-3">
                 <img src="https://usyrtqjsyizmjgpizckc.supabase.co/storage/v1/object/public/assets/nft%202.jfif" alt="Avatar" className="rounded-full w-12 h-12" />
                 <div>
-                  <span className="font-bold">Debussy.eth</span>
-                  <span className="block text-gray-500">@Debussy100</span>
+                  <span className="font-bold">Zeno_Web.eth</span>
+                  <span className="block text-gray-500">@Zeno_Web100</span>
                 </div>
               </div>
               <Twitter className="w-8 h-8 text-slate-500" />
             </div>
-            <p className="mb-4">
-              Just had a demo of <span className="text-slate-500">@blur_io</span> with{' '}
-              <span className="text-slate-500">@PacmanBlur</span> ...Holy moly, this platform will put the others to shame.
-              This is the future of NFT trading. I'm very very impressed with it.
+            <p className="mb-4 px-4">
+              Just had a demo of <span className="text-slate-500">@OmniPump.com</span> with{' '}
+              <span className="text-slate-500">@Zieco</span> ...Holy moly, this platform will put the others to shame.
+              This is the future of Token trading. I'm very very impressed with it.
             </p>
-            <span className="text-gray-500">8:01 PM · Aug 21, 2022</span>
+            <span className="text-gray-500 px-4">8:01 PM · Aug 21, 2022</span>
           </div>
         </motion.div>
 
@@ -93,19 +78,19 @@ export default function ImageContent1() {
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
-          <img alt='image1' src='/images/image.PNG' className='w-full h-full rounded-xl shadow-lg' />
+          <img alt='image1' src='/images/image2.PNG' className='w-full h-full shadow-lg' />
         </motion.div>
       </div>
 
       {/* Second Card */}
-      <div className="flex flex-col md:flex-row gap-10">
+      <div className="flex flex-col md:flex-row gap-20 ">
       <motion.div
           className="flex-1"
           initial={{ x: 100, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
-          <img alt='image2' src='/images/image.PNG' className='w-full h-full rounded-xl shadow-lg' />
+          <img alt='image2' src='/images/image.PNG' className='w-full h-full  shadow-lg' />
         </motion.div>
         <motion.div
           className="flex-1"
@@ -139,49 +124,34 @@ export default function ImageContent1() {
             </motion.span>
           </motion.p>
 
-          <div className="border border-slate-500 bg-black rounded-lg p-4 shadow-lg">
+          <div className="border border-slate-500 bg-black py-4 shadow-lg">
             <motion.p
-              className="leading-10 tracking-tight px-4 py-2 text-left hover:text-gray-500 text-md md:text-xl hover:scale-110 hover:text-xl md:hover:text-xl hover:-translate-y-1 transition-all duration-300 ease-in-out text-xl font-normal relative"
+              className="leading-10 tracking-tight px-4 py-2 text-left hover:text-gray-500 text-md md:text-xl hover:scale-110 hover:text-xl md:hover:text-xl hover:-translate-y-1 transition-all duration-300 ease-in-out text-xl font-normal "
               whileHover={{
                 y: [-2, 2, -2],
                 transition: { repeat: Infinity, duration: 0.5 }
               }}
             >
               @ZENO_WEB
-              <motion.span
-                className="absolute inset-0 text-transparent pointer-events-none"
-                style={{
-                  textShadow: `
-                    0 0 20px rgba(247, 242, 218, 0.7),
-                    0 0 40px rgba(247, 242, 218, 0.5),
-                    0 0 60px rgba(247, 242, 218, 0.3)
-                  `,
-                  WebkitTextStroke: "2px rgba(247, 242, 218, 0.2)",
-                }}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2, duration: 0.5 }}
-              >
-                @ZENO_WEB
-              </motion.span>
+           
             </motion.p>
             <hr className='bg-slate-500 border-slate-500 w-full mb-4' />
-            <div className="flex items-center justify-between gap-4 mb-4">
+            <div className="flex items-center justify-between gap-4 mb-4  px-4 ">
               <div className="flex items-center gap-3">
                 <img src="https://usyrtqjsyizmjgpizckc.supabase.co/storage/v1/object/public/assets/nft%202.jfif" alt="Avatar" className="rounded-full w-12 h-12" />
                 <div>
-                  <span className="font-bold">Debussy.eth</span>
-                  <span className="block text-gray-500">@Debussy100</span>
+                  <span className="font-bold">Zeno_Web.eth</span>
+                  <span className="block text-gray-500">@Zeno_Web100</span>
                 </div>
               </div>
               <Twitter className="w-8 h-8 text-slate-500" />
             </div>
-            <p className="mb-4">
-              Just had a demo of <span className="text-slate-500">@blur_io</span> with{' '}
-              <span className="text-slate-500">@PacmanBlur</span> ...Holy moly, this platform will put the others to shame.
-              This is the future of NFT trading. I'm very very impressed with it.
+            <p className="mb-4 px-4">
+              Just had a demo of <span className="text-slate-500">@OmniPump.com</span> with{' '}
+              <span className="text-slate-500">@Zieco</span> ...Holy moly, this platform will put the others to shame.
+              This is the future of Token trading. I'm very very impressed with it.
             </p>
-            <span className="text-gray-500">8:01 PM · Aug 21, 2022</span>
+            <span className="text-gray-500 px-4">8:01 PM · Aug 21, 2022</span>
           </div>
         </motion.div>
 
@@ -189,7 +159,7 @@ export default function ImageContent1() {
         
       </div>
       {/* Repeat similar structure for additional cards */}
-      <div className="flex flex-col md:flex-row gap-10">
+      <div className="flex flex-col md:flex-row gap-20">
         <motion.div
           className="flex-1"
           initial={{ x: -100, opacity: 0 }}
@@ -222,49 +192,34 @@ export default function ImageContent1() {
             </motion.span>
           </motion.p>
 
-          <div className="border border-slate-500 bg-black rounded-lg p-4 shadow-lg">
+          <div className="border border-slate-500 bg-black   py-4 shadow-lg">
             <motion.p
-              className="leading-10 tracking-tight px-4 py-2 text-left hover:text-gray-500 text-md md:text-xl hover:scale-110 hover:text-xl md:hover:text-xl hover:-translate-y-1 transition-all duration-300 ease-in-out text-xl font-normal relative"
+              className="leading-10 tracking-tight px-4 py-2 text-left hover:text-gray-500 text-md md:text-xl hover:scale-110 hover:text-xl md:hover:text-xl hover:-translate-y-1 transition-all duration-300 ease-in-out text-xl font-normal "
               whileHover={{
                 y: [-2, 2, -2],
                 transition: { repeat: Infinity, duration: 0.5 }
               }}
             >
               @ZENO_WEB
-              <motion.span
-                className="absolute inset-0 text-transparent pointer-events-none"
-                style={{
-                  textShadow: `
-                    0 0 20px rgba(247, 242, 218, 0.7),
-                    0 0 40px rgba(247, 242, 218, 0.5),
-                    0 0 60px rgba(247, 242, 218, 0.3)
-                  `,
-                  WebkitTextStroke: "2px rgba(247, 242, 218, 0.2)",
-                }}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2, duration: 0.5 }}
-              >
-                @ZENO_WEB
-              </motion.span>
+            
             </motion.p>
             <hr className='bg-slate-500 border-slate-500 w-full mb-4' />
-            <div className="flex items-center justify-between gap-4 mb-4">
+            <div className="flex items-center justify-between gap-4 mb-4 px-4">
               <div className="flex items-center gap-3">
                 <img src="https://usyrtqjsyizmjgpizckc.supabase.co/storage/v1/object/public/assets/nft%202.jfif" alt="Avatar" className="rounded-full w-12 h-12" />
                 <div>
-                  <span className="font-bold">Debussy.eth</span>
-                  <span className="block text-gray-500">@Debussy100</span>
+                  <span className="font-bold">Zeno_Web.eth</span>
+                  <span className="block text-gray-500">@Zeno_Web100</span>
                 </div>
               </div>
               <Twitter className="w-8 h-8 text-slate-500" />
             </div>
-            <p className="mb-4">
-              Just had a demo of <span className="text-slate-500">@blur_io</span> with{' '}
-              <span className="text-slate-500">@PacmanBlur</span> ...Holy moly, this platform will put the others to shame.
-              This is the future of NFT trading. I'm very very impressed with it.
+            <p className="mb-4 px-4">
+              Just had a demo of <span className="text-slate-500">@OmniPump.com</span> with{' '}
+              <span className="text-slate-500">@Zieco</span> ...Holy moly, this platform will put the others to shame.
+              This is the future of Token trading. I'm very very impressed with it.
             </p>
-            <span className="text-gray-500">8:01 PM · Aug 21, 2022</span>
+            <span className="text-gray-500 px-4">8:01 PM · Aug 21, 2022</span>
           </div>
         </motion.div>
 
@@ -275,7 +230,7 @@ export default function ImageContent1() {
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
-          <img alt='image1' src='/images/image3.PNG' className='w-full h-full rounded-xl shadow-lg' />
+          <img alt='image3' src='/images/image3.PNG' className='w-full h-full shadow-lg' />
         </motion.div>
       </div>
     </div>
