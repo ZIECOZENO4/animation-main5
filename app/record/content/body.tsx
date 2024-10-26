f6aa51f7a5723f6c24917da4c056601481248400
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-27 00:18:11 +0100

    ghccvhgv

--- app/record/content/body.tsx
+++ app/record/content/body.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useState } from "react";
 import { motion } from 'framer-motion';
 import { Card, Chip, Tabs, Tab } from "@nextui-org/react";
 import FullConnectButton from '@/components/fullConnectButton';
