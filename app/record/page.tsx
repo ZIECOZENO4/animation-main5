7efa4610f2645d5963ab76bef3459e2663b1ff13
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-26 19:14:26 +0100

    sdgdfhgf

--- app/record/page.tsx
+++ app/record/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import Dashboard from "./content/body";
 import TokenHeader from "./content/header";
 
 export default function Home() {
   return (
     <main className="">
  <TokenHeader />
+ <Dashboard />
     </main>
   );
 }
