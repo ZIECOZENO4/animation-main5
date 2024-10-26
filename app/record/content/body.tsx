fa82e7ec6afa9a5bdedb1765d83e789022e746ff
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-27 00:16:20 +0100

    jhjh

--- app/record/content/body.tsx
+++ app/record/content/body.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from 'react';
 import { motion } from 'framer-motion';
+import { Card, Chip, Tabs, Tab } from "@nextui-org/react";
+import FullConnectButton from '@/components/fullConnectButton';
 
 export default function Dashboard() {
   const [expandedLeft, setExpandedLeft] = React.useState(false);
@@ -9,7 +11,11 @@ export default function Dashboard() {
   const [expandedLeftBottom, setExpandedLeftBottom] = React.useState(false);
   const [expandedRightTop, setExpandedRightTop] = React.useState(false);
   const [expandedRightBottom, setExpandedRightBottom] = React.useState(false);
+  const [selectedCurrency, setSelectedCurrency] = useState('USD');
 
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCurrency(event.target.value);
+  };
   // Calculate widths based on expansion states
   const leftPanelWidth = expandedLeft ? 'w-1/10' : 'w-1/4'; // 20px when expanded
   const rightPanelWidth = expandedRight ? 'w-1/10' : 'w-1/4'; // 20px when expanded
@@ -22,14 +28,75 @@ export default function Dashboard() {
       {/* Left Panel */}
       <div className={`flex flex-col border-r transition-all duration-300 ${leftPanelWidth}`}>
         <motion.div
-          className={`p-2 transition-all duration-300 ${expandedLeftTop ? 'h-1/2' : 'h-1/4'}`}
+          className={` transition-all duration-300 ${expandedLeftTop ? 'h-1/2' : 'h-1/4'}`}
           onClick={() => setExpandedLeftTop(!expandedLeftTop)}
         >
           <div className="cursor-pointer">
             {expandedLeftTop ? '▼' : '►'} Top
           </div>
-          {/* Content Here */}
+          <Card className="bg-black border rounded-none border-slate-600 mt-2 p-4">
+  
+            <div className="flex text-xs justify-between py-2">
+              <span>Total Supply:10001</span>
+              <span className=" text-xs text-gray-400">1 $1 is 0.000010 ETH ($0.023)</span>
+            </div>
+            <div className="flex justify-center align-middle w-full mb-4 p-2">
+            <Tabs 
+                   className='w-full'
+                   classNames={{
+                     tabList: "w-full",
+                     tab: "flex-1",
+                     cursor: "w-full",
+                   }}
+            key="transaction" variant="bordered" aria-label="Tabs variants">
+          <Tab key="BUY" title="BUY" />
+          <Tab key="SELL" title="SELL" />
+        </Tabs>
+ 
+            </div>
+            <div className="flex text-xs justify-between  mb-4">
+              <span>Total Supply:10001</span>
+              <span className="text-slate-400">Set slippage: 1%</span>
+            </div>
+            <div className="flex gap-4 mb-4">
+              <input
+                type="text"
+                placeholder="Enter amount"
+                className="flex-grow bg-black border border-gray-700 p-2 rounded-xl"
+              />
+               <select
+      value={selectedCurrency}
+      onChange={handleChange}
+      aria-label="Select Currency"
+      className="bg-black text-[#F7F2DA] border border-gray-700 p-2 rounded-xl"
+    >
+      <option value="USD">USD</option>
+      <option value="ETH">ETH</option>
+      <option value="BTC">BTC</option>
+      <option value="SOL">SOL</option>
+    </select>
+            </div>
+            <div className="flex flex-grow space-x-2 mb-4">
+              <Chip radius="sm" className="flex-grow p-2">
+                1 ETH
+              </Chip>
+              <Chip radius="sm" className="flex-grow p-2">
+                5 ETH
+              </Chip>
+              <Chip radius="sm" className="flex-grow p-2">
+                10 ETH
+              </Chip>
+              
+         
+            </div>
+     
+            <div className="w-full">
+            <FullConnectButton />
+            </div>
+         
+          </Card>
         </motion.div>
+        
         <motion.div
           className={`p-2 transition-all duration-300 ${expandedLeftBottom ? 'h-full' : 'h-1/2'}`}
           onClick={() => setExpandedLeftBottom(!expandedLeftBottom)}
