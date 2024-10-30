b61d9466f79191d3deae045f280678ce3e934ed2
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-30 10:08:57 +0100

    jhkvyhvbh

--- components/MdNavBar.tsx
+++ components/MdNavBar.tsx
@@ -9,8 +9,28 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "@nextui-org/react";
 
+type ActiveItem = string | null;
+
+interface NavItem {
+  name: string;
+  path: string;
+}
+const styles = {
+  navLink: `text-[#F7F2DA] flex mx-[10px]`,
+  badge: `rounded-full bg-blue-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4`,
+  navItem: `relative mr-1 cursor-pointer hover:opacity-60`,
+  nav: `flex justify-center items-center gap-[20px]`,
+  headerWrapper: `md:flex md:justify-between h-full max-w-screen-xl mx-auto px-4 hidden`,
+  inputContainer: `flex items-center justify-center p-2 rounded `,
+  input: `bg-transparent outline-none text-[#F7F2DA] w-70 ml-3`,
+  cursorPointer: `mr-5 cursor-pointer`,
+};
+
 const MdNavBar = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  const [activeItem, setActiveItem] = useState<ActiveItem>(null);
+    const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+    const currentPath = usePathname();
+    const [isVisible, setIsVisible] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
   const { scrollY } = useScroll();
 
@@ -42,29 +62,6 @@ const MdNavBar = () => {
     }
   };
 
-
-type ActiveItem = string | null;
-
-interface NavItem {
-  name: string;
-  path: string;
-}
-const styles = {
-  navLink: `text-[#F7F2DA] flex mx-[10px]`,
-  badge: `rounded-full bg-blue-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4`,
-  navItem: `relative mr-1 cursor-pointer hover:opacity-60`,
-  nav: `flex justify-center items-center gap-[20px]`,
-  headerWrapper: `md:flex md:justify-between h-full max-w-screen-xl mx-auto px-4 hidden`,
-  inputContainer: `flex items-center justify-center p-2 rounded `,
-  input: `bg-transparent outline-none text-[#F7F2DA] w-70 ml-3`,
-  cursorPointer: `mr-5 cursor-pointer`,
-};
-
-const MdNavBar = () => {
-  const [activeItem, setActiveItem] = useState<ActiveItem>(null);
-    const [isNotificationOpen, setIsNotificationOpen] = useState(false);
-    const currentPath = usePathname();
-  
     const navItems: NavItem[] = [
       { name: "Home", path: "/" },
       { name: "Create", path: "/create" },
