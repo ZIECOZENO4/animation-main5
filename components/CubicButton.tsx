bc5dd354e2aa90ba14ac0cc46a019a494b9e9144
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-30 07:05:15 +0100

    vhhgv

--- components/CubicButton.tsx
+++ components/CubicButton.tsx
@@ -43,31 +43,7 @@ const CubicButton = () => {
             {(() => {
               if (!connected) {
                 return (
-              //     <motion.div 
-              //       className='border-[0.63px] border-[#000000] mx-4 shake-button cursor-pointer'
-              //    
-              //       initial={{ scale: 0 }}
-              //       animate={{ scale: 1 }}
-                
-              //       whileHover={{ scale: 1.05 }}
-              //     >
-              //            <div className="flex">
-              //            <div className="w-[2.84px] h-[36.22px] bg-[#787878] border-t-[0.63px] border-black" />
-              //   <div className="flex items-center justify-center w-[2.84px] h-[36.22px] bg-[#787878]">
-              //     <motion.p 
-              //       className="text-[20px] text-[#F7F2DA] font-normal"
-              //       initial={{ opacity: 0 }}
-              //       animate={{ opacity: 1 }}
-            
-              //     >
-              //         CONNECT
-              //     </motion.p>
-              //   </div>
-              // </div>
-              // <div className="w-w-[] h-[2px] bg-[#787878] border-t-[0.63px] border-[#000000]" />
-              //            </div>
-                  
-              //     </motion.div>
+
               <motion.div 
               className="w-[110.08px] h-[36.22px] shake-button border-[0.63px] border-[#000000]"
               initial={{ scale: 0 }}
@@ -88,21 +64,31 @@ const CubicButton = () => {
                   </motion.p>
                 </div>
               </div>
-              <div className="w-[110.08px] pr-1 h-[2px] bg-[#787878] border-t-[0.63px] border-[#000000]" />
+              <div className="w-[110px]  h-[2px] bg-[#787878] border-t-[0.63px] border-[#000000]" />
             </motion.div>
                 )
               }
 
               return (
                 <div className="flex flex-col sm:flex-row gap-1">
-                    <div 
-                    className='flex flex-row mx-1 w-auto shake-button cursor-pointer'
-                    onClick={openChainModal}
+                   
+                  <motion.div 
+              className="w-auto h-[36.22px] shake-button border-[0.63px] border-[#000000]"
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              onClick={openChainModal}
+              whileHover={{ scale: 1.05 }}
+            >
+              <div className="flex">
+                <div className="w-[2.84px] h-[36.22px] bg-[#787878] border-t-[0.63px] border-black" />
+                <div className=" w-auto px-2 shadow-md flex items-center justify-between h-[33px] bg-[#787878]">
+                  <motion.p 
+                    className="text-[20px] text-[#F7F2DA] text-center font-normal"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+        
                   >
-                    <div className="w-[2.84px] h-full bg-[#787878] border-t-[0.63px] border-solid border-black"></div>
-                    <div className='flex flex-col'>
-                      <div className="w-auto px-2 h-[45px] bg-[#787878] shadow-md flex items-center justify-between ">
-                        <div className="flex items-center gap-2">
+              <div className="flex items-center gap-2">
                           {chain.iconUrl && (
                             <Image
                               src={chain.iconUrl}
@@ -124,20 +110,30 @@ const CubicButton = () => {
                         )}
                           <ChevronDownIcon />
                         </div>
-                     
-                      </div>
-                      <div className="w-full h-[3.15px] bg-[#787878] border-t-[0.63px] border-solid border-black"></div>
-                    </div>
-                  </div>
+                  </motion.p>
+                </div>
+              </div>
+              <div className="w-full  h-[2px] bg-[#787878] border-t-[0.63px] border-[#000000]" />
+            </motion.div>
                   {/* Account & Balance Button */}
-                  <div 
-                    className='flex flex-row mx-2 w-auto shake-button cursor-pointer'
-                    onClick={openAccountModal}
+      
+                  <motion.div 
+              className="w-auto h-[36.22px] shake-button border-[0.63px] border-[#000000]"
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              onClick={openAccountModal}
+              whileHover={{ scale: 1.05 }}
+            >
+              <div className="flex">
+                <div className="w-[2.84px] h-[36.22px] bg-[#787878] border-t-[0.63px] border-black" />
+                <div className=" w-auto px-2 shadow-md flex items-center justify-between h-[33px] bg-[#787878]">
+                  <motion.p 
+                    className="text-[20px] text-[#F7F2DA] text-center font-normal"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+        
                   >
-                    <div className="w-[2.84px] h-full bg-[#787878] border-t-[0.63px] border-solid border-black"></div>
-                    <div className='flex flex-col'>
-                      <div className="w-auto px-2 h-[45px] bg-[#787878] shadow-md flex items-center justify-between ">
-                        <div className="flex items-center gap-2">
+                  <div className="flex items-center gap-2">
                           {account.ensAvatar && (
                             <Image
                               src={account.ensAvatar}
@@ -155,11 +151,11 @@ const CubicButton = () => {
                             {account.displayName}
                           </span>
                         <ChevronDownIcon />
-                      </div>
-                      <div className="w-full h-[3.15px] bg-[#787878] border-t-[0.63px] border-solid border-black"></div>
-                    </div>
-                  </div>
-
+                  </motion.p>
+                </div>
+              </div>
+              <div className="w-full  h-[2px] bg-[#787878] border-t-[0.63px] border-[#000000]" />
+            </motion.div>
                   {/* Network Button */}
                 
                 </div>
