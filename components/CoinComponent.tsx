215385212269205cfec63ba6abf092493197be32
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-31 23:29:18 +0100

    klmkm

--- components/CoinComponent.tsx
+++ components/CoinComponent.tsx
@@ -21,231 +21,17 @@ import CardDemo from "./CardDemo";
 import CardGrid from "./Test";
 import "./WorkbenchFontTest.css";
 import Link from "next/link";
-const chainData = [
-  { key: "all", name: "All Chains" },
-  {
-    key: "ethereum",
-    name: "Ethereum",
-    icon: SiEthereum,
-    networks: ["Mainnet", "Goerli", "Sepolia"]
-  },
-  {
-    key: "binance",
-    name: "Binance Smart Chain",
-    icon: SiBinance,
-    networks: ["BSC", "BSC Testnet"]
-  },
-  {
-    key: "polygon",
-    name: "Polygon",
-    icon: SiPolygon,
-    networks: ["Mainnet", "Mumbai"]
-  },
-  {
-    key: "arbitrum",
-    name: "Arbitrum",
-    icon: SiEthereum,
-    networks: ["One", "Nova", "Goerli"]
-  },
-  {
-    key: "avalanche",
-    name: "Avalanche",
-    icon: SiEthereum,
-    networks: ["C-Chain", "Fuji"]
-  },
-  {
-    key: "optimism",
-    name: "Optimism",
-    icon: SiEthereum,
-    networks: ["Mainnet", "Goerli"]
-  },
-  {
-    key: "fantom",
-    name: "Fantom",
-    icon: SiEthereum,
-    networks: ["Opera", "Testnet"]
-  },
-  {
-    key: "cronos",
-    name: "Cronos",
-    icon: SiEthereum,
-    networks: ["Mainnet", "Testnet"]
-  },
-  { key: "gnosis", name: "Gnosis", icon: SiEthereum, networks: ["Mainnet"] },
-  {
-    key: "base",
-    name: "Base",
-    icon: SiEthereum,
-    networks: ["Mainnet", "Goerli"]
-  }
-];
-const coins = [
-  {
-    id: 1,
-    name: "Bitcoin",
-    symbol: "BTC",
-    value: 62981.57,
-    market_cap: 1240000000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
-  },
-  {
-    id: 2,
-    name: "Ethereum",
-    symbol: "ETH",
-    value: 2542.83,
-    market_cap: 310490000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png"
-  },
-  {
-    id: 3,
-    name: "Tether",
-    symbol: "USDT",
-    value: 1.0,
-    market_cap: 119160000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/825.png"
-  },
-  {
-    id: 4,
-    name: "BNB",
-    symbol: "BNB",
-    value: 568.79,
-    market_cap: 84550000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png"
-  },
-  {
-    id: 5,
-    name: "Solana",
-    symbol: "SOL",
-    value: 145.86,
-    market_cap: 68330000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png"
-  },
-  {
-    id: 6,
-    name: "XRP",
-    symbol: "XRP",
-    value: 0.583,
-    market_cap: 32870000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/52.png"
-  },
-  {
-    id: 7,
-    name: "Cardano",
-    symbol: "ADA",
-    value: 0.62,
-    market_cap: 21800000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png"
-  },
-  {
-    id: 8,
-    name: "Dogecoin",
-    symbol: "DOGE",
-    value: 0.105,
-    market_cap: 15290000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/74.png"
-  },
-  {
-    id: 9,
-    name: "Polkadot",
-    symbol: "DOT",
-    value: 7.92,
-    market_cap: 10340000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6636.png"
-  },
-  {
-    id: 10,
-    name: "TRON",
-    symbol: "TRX",
-    value: 0.152,
-    market_cap: 13590000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1958.png"
-  },
-  {
-    id: 11,
-    name: "Chainlink",
-    symbol: "LINK",
-    value: 15.23,
-    market_cap: 8620000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1975.png"
-  },
-  {
-    id: 12,
-    name: "Polygon",
-    symbol: "MATIC",
-    value: 0.89,
-    market_cap: 8580000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png"
-  },
-  {
-    id: 13,
-    name: "Litecoin",
-    symbol: "LTC",
-    value: 86.51,
-    market_cap: 6390000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2.png"
-  },
-  {
-    id: 14,
-    name: "Dai",
-    symbol: "DAI",
-    value: 1.0,
-    market_cap: 4900000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png"
-  },
-  {
-    id: 15,
-    name: "Shiba Inu",
-    symbol: "SHIB",
-    value: 0.00001,
-    market_cap: 6100000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5994.png"
-  },
-  {
-    id: 16,
-    name: "Avalanche",
-    symbol: "AVAX",
-    value: 16.92,
-    market_cap: 5500000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5805.png"
-  },
-  {
-    id: 17,
-    name: "Uniswap",
-    symbol: "UNI",
-    value: 5.97,
-    market_cap: 3460000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png"
-  },
-  {
-    id: 18,
-    name: "Stellar",
-    symbol: "XLM",
-    value: 0.16,
-    market_cap: 4420000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/512.png"
-  },
-  {
-    id: 19,
-    name: "Monero",
-    symbol: "XMR",
-    value: 163.21,
-    market_cap: 2980000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/328.png"
-  },
-  {
-    id: 20,
-    name: "Bitcoin Cash",
-    symbol: "BCH",
-    value: 238.84,
-    market_cap: 4670000000,
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1831.png"
-  }
-];
+
+interface ChainData {
+  key: string
+  name: string
+}
+
+
 
 export default function ComponentCoin() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [filteredCoins, setFilteredCoins] = useState(coins);
-  const [selectedChain, setSelectedChain] = useState("all");
+  const [selectedChain, setSelectedChain] = useState("All Chains");
   const [minMarketCap, setMinMarketCap] = useState("");
   const [maxMarketCap, setMaxMarketCap] = useState("");
   const [liked, setLiked] = React.useState(false);
@@ -260,45 +46,12 @@ export default function ComponentCoin() {
   const [minPrice, setMinPrice] = useState('')
   const [maxPrice, setMaxPrice] = useState('')
 
-  
-  useEffect(() => {
-    applyFilters();
-  }, [selectedChain, selectedMarketCap, minMarketCap, maxMarketCap]);
-
-  const applyFilters = () => {
-    let filtered = coins;
-
-    if (selectedMarketCap !== "all") {
-      switch (selectedMarketCap) {
-        case "low":
-          filtered = filtered.filter((coin) => coin.market_cap < 1000000000);
-          break;
-        case "mid":
-          filtered = filtered.filter(
-            (coin) =>
-              coin.market_cap >= 1000000000 && coin.market_cap < 10000000000
-          );
-          break;
-        case "high":
-          filtered = filtered.filter((coin) => coin.market_cap >= 10000000000);
-          break;
-      }
-    }
-
-    if (minMarketCap !== "") {
-      filtered = filtered.filter(
-        (coin) => coin.market_cap >= parseFloat(minMarketCap) * 1000000000
-      );
-    }
-
-    if (maxMarketCap !== "") {
-      filtered = filtered.filter(
-        (coin) => coin.market_cap <= parseFloat(maxMarketCap) * 1000000000
-      );
-    }
-
-    setFilteredCoins(filtered);
-  };
+  const chainData: ChainData[] = [
+    { key: 'all', name: 'All Chains' },
+    { key: 'ethereum', name: 'Ethereum' },
+    { key: 'arbitrum', name: 'Arbitrum' },
+    { key: 'optimisim', name: 'Optimisim' },
+  ]
 
   const resetFilters = () => {
     setSelectedChain("all");
@@ -355,7 +108,7 @@ export default function ComponentCoin() {
         {/* Desktop view */}
         <div className="hidden md:flex items-center gap-4">
       {/* Chain Dropdown */}
-      <div className="relative w-48">
+      <div className="relative w-[20rem]">
         <motion.button
           onClick={() => setIsChainOpen(!isChainOpen)}
           className="w-full px-4 py-2 text-left bg-[#0A0909] border-2 border-[#1a1a1a] 
@@ -418,7 +171,7 @@ export default function ComponentCoin() {
       </div>
 
       {/* Market Cap Dropdown */}
-      <div className="relative w-[40rem]">
+      <div className="relative w-[20rem]">
         <motion.button
           onClick={() => setIsMarketCapOpen(!isMarketCapOpen)}
           className="w-full px-4 py-2 text-left bg-[#0A0909] border-2 border-[#1a1a1a] 
@@ -481,7 +234,7 @@ export default function ComponentCoin() {
       </div>
 
       {/* Price Filter Dropdown */}
-      <div className="relative w-48">
+      <div className="relative w-[20rem]">
         <motion.button
           onClick={() => setIsPriceOpen(!isPriceOpen)}
           className="w-full px-4 py-2 text-left bg-[#0A0909] border-2 border-[#1a1a1a] 
@@ -919,7 +672,7 @@ export default function ComponentCoin() {
                 <Button
                   color="primary"
                   onPress={() => {
-                    applyFilters();
+           
                     onClose();
                   }}
                 >
