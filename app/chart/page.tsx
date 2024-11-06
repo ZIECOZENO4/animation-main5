b0dacde1346cf00303b89845d0be6c7dc4ea24b6
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-06 16:34:03 +0100

    34t54t

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -4,8 +4,8 @@ import DeptComponent from './dept-graph'
 
 const ChartPage = () => {
   return (
-    <div className='bg-black p-4 min-h-screen w-full flex justify-between'> <div className='w-[60%]'>
-    <SalesChart />    </div> <div className='w-[40%]'><DeptComponent /></div> </div>
+    <div className='bg-black p-4 min-h-screen w-full flex justify-between'> <div className='w-[40%]'>
+    <SalesChart />    </div> <div className='w-[60%]'><DeptComponent /></div> </div>
   )
 }
 
