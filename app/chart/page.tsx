6f5f6437ab19c19f84c1db5ce362a82a5c430ccf
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-07 16:01:01 +0100

    iohiujn

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,21 +1,8 @@
 import React from 'react'
-import SalesChart from './sales-chart'
-import DeptComponent from './dept-graph'
 import EnhanceTradingView from './enhanced-trading-interface'
 
 const ChartPage = () => {
   return (
-    // <div className='bg-black p-4 min-h-screen flex-col md:flex-row gap-4 w-full flex justify-between'> 
-    // <div className='w-full md:w-[60%]'>
-    // <SalesChart />   
-    //  </div> 
-    //  <div className='w-full md:w-[40%]'>
-    //     <DeptComponent />
-
-
-        
-    //     </div>
-    //      </div>
     <div className="m-0 p-0">
       <EnhanceTradingView />
     </div>
