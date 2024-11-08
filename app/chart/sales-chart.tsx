bc44d987ee139b62f9f4b4cdc87faf9aa2ab0cf4
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-08 14:59:11 +0100

    jhvhjb

--- app/chart/sales-chart.tsx
+++ app/chart/sales-chart.tsx
@@ -342,7 +342,7 @@ export default function SalesChart() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-        className="w-[calc(100vw-40px)] h-[calc(100vh-140px)]  container mx-auto bg-black p-4 relative"
+        className="w-[calc(100vw-35vw)] h-[calc(100vh-63vh)] container mx-auto bg-black p-4 relative"
       >
       
         
