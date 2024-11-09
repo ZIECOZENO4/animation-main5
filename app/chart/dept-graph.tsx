25197c1df57498c929c0f8a6d4a997d33189193b
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-09 20:21:49 +0100

    jkbj

--- app/chart/dept-graph.tsx
+++ app/chart/dept-graph.tsx
@@ -166,7 +166,7 @@ export default function DeptComponent() {
   }
 
   return (
-    <div className="w-[calc(100vw-35vw)] h-[calc(100vh-61vh)]">
+    <div className="w-[calc(100vw-35vw)] h-[calc(100vh-60vh)]">
       <motion.div 
         initial={{ opacity: 0 }}
         animate={{ opacity: isLoaded ? 1 : 0 }}
