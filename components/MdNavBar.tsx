97f64d207e0763200df469476f07884a6328a9c0
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-11 15:57:36 +0100

    ghuyh

--- components/MdNavBar.tsx
+++ components/MdNavBar.tsx
@@ -118,7 +118,7 @@ const MdNavBar = () => {
             </Link>
 
             {/* Center Section: Navigation Items */}
-            <div className="flex-grow flex items-center  pl-[20%]  justify-center">
+            <div className="flex-grow flex items-center  pl-[18%]  justify-center">
                 <nav className="flex items-center">
                     {navItems.map((item) => (
                         <motion.div
