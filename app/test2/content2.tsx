2d4bf2de7620a6fd755a5fcfd1eb9095e10978af
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-11 13:53:48 +0100

    hjgvkh

--- app/test2/content2.tsx
+++ app/test2/content2.tsx
@@ -73,7 +73,7 @@ export default function ExamplePage() {
             `,
           }}
         />
-        {/* Right border */}
+
         <div 
           className="absolute -right-[4px] -top-[4px] -bottom-[4px]"
           style={{
