2584bdb2f8b8665c8b87ef31b1fe878869be7d7e
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-16 16:42:25 +0100

    jkbjn

--- app/test/content.tsx
+++ app/test/content.tsx
@@ -475,8 +475,7 @@ const BaseTokenSelect: React.FC = () => {
   return (
     <>
          <p className='text-xs text-[#F7F2DA40]'>To</p>
-      <div className="flex gap-4">
- 
+      <div className="flex gap-2">
         <div className="w-[100%] h-[51px] bg-[#5555554D]">
           <BorderComponent>
             <div className="flex justify-between items-center h-full px-4">
