6cc32ee7611a4fae93eac6f77378a01296f16d5c
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-16 08:00:52 +0100

    jhbj

--- app/test/content.tsx
+++ app/test/content.tsx
@@ -1406,10 +1406,10 @@ const removeToken = (symbol: string) => {
             className="h-full  w-[30%]  bg-[#000000]"
           >
             <BorderComponent>
-              <div className="flex flex-col h-full overflow-y-scroll p-6 gap-4">
+              <div className="flex flex-col h-full  p-6 gap-4">
                 {/* Top Section */}
 
-                <div className="space-y-4 h-[65%]">
+                <div className="space-y-4 h-[65%] overflow-y-scroll">
                   {/* First Token Input */}
                   {/* <div className="flex gap-4">
                     <div className="w-[80%] h-[51px] bg-[#5555554D]">
@@ -1627,7 +1627,7 @@ const removeToken = (symbol: string) => {
                 </div>
 
                 {/* Bottom Section */}
-                <div className="flex flex-col justify-between h-[35%]">
+                <div className="flex flex-col justify-between h-[35%] overflow-y-scroll">
                   {" "}
                   {/* Changed from space-y-4 to flex with justify-between */}
                   <div className="space-y-4">
