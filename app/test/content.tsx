84255610bc179b7900ed8d9d34f2bea6cbb09e98
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-18 23:08:52 +0100

    nm mn

--- app/test/content.tsx
+++ app/test/content.tsx
@@ -1544,7 +1544,7 @@ export default function TestContent() {
           {/* Right Column */}
           <motion.div
             initial={{ opacity: 0, x: 50 }}
-            animate={{ opacity: 1, x: 0 }}m
+            animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5, ease: "easeOut" }}
             className="h-[100%]   w-[30%]  bg-[#000000]"
           >
