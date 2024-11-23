ef458f54257dc0fed661fb42b3cd370f0a00be1d
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-23 18:59:01 +0100

    jbljbn

--- components/CoinComponent.tsx
+++ components/CoinComponent.tsx
@@ -1097,41 +1097,41 @@ const Card = () => (
     transition={{ duration: 0.5, delay: 0.1 }}
     whileHover={{ scale: 1.07 }}
   >
-     <Tooltip 
-  content="Still in Initial Stage"
-  placement="top"
-  showArrow={false}
-  offset={-5}
-  classNames={{
-    base: [
-      "py-2 px-4",
-      "border-none",
-      "shadow-none",
-      "backdrop-blur-none",
-      "relative",
-      "mb-2  " // Added margin bottom
-    ].join(" "),
-    content: [
-      "text-[#F7F2DA]",
-      "text-sm",
-      "font-normal",
-      "px-2 py-1",
-      "rounded-none"
-    ].join(" ")
-  }}
-  motionProps={{
-    variants: {
-      exit: {
-        opacity: 0,
-        transition: { duration: 0.1, ease: "easeIn" }
-      },
-      enter: {
-        opacity: 1,
-        transition: { duration: 0.15, ease: "easeOut" }
-      }
-    }
-  }}
->
+           <Tooltip
+        content="Still in Initial Stage"
+        placement="top"
+        showArrow={false}
+        offset={-5}
+        classNames={{
+          base: [
+            "py-2 px-4",
+            "border-none",
+            "shadow-none",
+            "backdrop-blur-none",
+            "relative",
+            "mb-2 tooltip-custom" // Custom class for styling
+          ].join(" "),
+          content: [
+            "text-[#F7F2DA]", // Text color
+            "text-sm",
+            "font-normal",
+            "px-2 py-1",
+            "rounded-none"
+          ].join(" ")
+        }}
+        motionProps={{
+          variants: {
+            exit: {
+              opacity: 0,
+              transition: { duration: 0.1, ease: "easeIn" }
+            },
+            enter: {
+              opacity: 1,
+              transition: { duration: 0.15, ease: "easeOut" }
+            }
+          }
+        }}
+      >
 
 <div
       className="bg-[#0A0909]  overflow-hidden"
