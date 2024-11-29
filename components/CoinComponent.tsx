e39f50b2f444fb11837ff4fa21c9b70637c582a4
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-11-29 14:35:40 +0100

    klmkm

--- components/CoinComponent.tsx
+++ components/CoinComponent.tsx
@@ -204,11 +204,11 @@ const truncateDescription = (description: string) => {
 };
 
 const TokenGrid = ({ tokens, activeTab }: { tokens: FormattedToken[], activeTab: 'Initial' | 'Anonymous' }) => (
-    <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4 w-auto">
         {tokens.map((token) => (
-
+           <Link href='/test' key={token.id} >
                 <motion.div
-            key={token.id}
+           
             className="w-full md:w-[350px] px-2 mb-4 relative"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -446,6 +446,7 @@ const TokenGrid = ({ tokens, activeTab }: { tokens: FormattedToken[], activeTab:
         </Tooltip>
         
           </motion.div>
+           </Link>
        
         ))}
     </div>
